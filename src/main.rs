8c68e4f3c44fdbbef5203673bc86456ff53c791a
jonghyeon <kiri6077@naver.com>
2025-03-23 17:46:48 +0900

    [rust] log -> tracing으로 로깅 디펜던시 변경

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,24 @@ use std::net::TcpListener;
 use rust_news::startup::run;
 use rust_news::configuration::get_configuration;
 use sqlx::PgPool;
-use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // 모든 log 이벤트를 구독자에게 리다이렉트함 
+    LogTracer::init().expect("Failed to set logger");
     // init()는 set_logger()를 호출
     // RUST_LOG 환경변수가 설정되어 있지 않으면
-    // info 및 그 이상의 모든 레벨의 로그를 출력함
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // info 및 그 이상의 모든 레벨의 span을 출력
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // 포맷이 적용된 span들을 stdout으로 출력
+    let formatting_layer = BunyanFormattingLayer::new("rust_news".into(), std::io::stdout);
+    let subscriber = Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer);
+    // 에플리케이션이 span을 처리할 수 있도록 subscriber 지정 
+    set_global_default(subscriber).expect("Failed to set subscriber");
     // 구성 읽기
     let config = get_configuration().expect("Failed to get configuration");
     let connection_pool = PgPool::connect(
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -16,14 +17,15 @@ pub async fn subscribe(
 ) -> HttpResponse {
     // 무작위 고유 식별자 생성
     let req_id = Uuid::new_v4();
-    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber.",
-        req_id,
-        form.email,
-        form.name);
-    log::info!(
-        "request_id {} - Saving new subscriber detrails in the database",
-        req_id
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        reuqest_id = %req_id, 
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber detrails in the database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -35,15 +37,16 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(connection.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", req_id);
+            tracing::info!("request_id {} - New subscriber details have been saved", req_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
             // println!("Faile to execute query: {}", e);
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
